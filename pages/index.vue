67197679a338250822591d41dce60545891128b5
VinGitonga <gitongavincent64@gmail.com>
2024-12-13 07:33:49 +0300

    added index redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { Button } from "~/components/ui/button";
+
+const { status } = useAuth();
+const computedStatus = computed(() => status.value);
 const router = useRouter();
 </script>
 <template>
 	<div class="h-dvh flex items-center justify-center">
 		<div class="text-center space-y-3">
 			<h1 class="text-center text-5xl font-semibold">Welcome to CarePulse</h1>
-			<Button @click="router.push('/auth')">Get Started</Button>
+			<Button @click="router.push(computedStatus === 'authenticated' ? '/admin' : '/auth')">
+				{{ computedStatus === "authenticated" ? "Dashboard" : "Get Started" }}
+			</Button>
 		</div>
 	</div>
 </template>
